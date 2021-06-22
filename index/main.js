faf0f9b5559f1242376a96a21de7039742a20a87
Artem Mozhegov <amozhegov@yahoo.com>
2021-06-22 18:47:44 +0300

    Uploaded index.html and main.js
    Created 3 input boxes on the main page and variables in js file

---
+++ index/main.js
@@ -0,0 +1,5 @@
+// variables from text user inputs
+
+const totalCost = document.getElementById("total-cost"),
+      anInitialFee = document.getElementById("an-initial-fee"),
+      creditTerm = document.getElementById("credit-term");
