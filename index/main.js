44455af71b3abc01bd27f72440ba4252bd6e0b47
Artem Mozhegov <amozhegov@yahoo.com>
2021-06-23 00:09:57 +0300

    Updated index.html and main.js
    Created 6 additional variables in the main.js and added div elements corresponding to the result sections in the index.html

--- index/main.js
+++ index/main.js
@@ -1,5 +1,20 @@
-// variables from text user inputs
+// variables for text user inputs
 
 const totalCost = document.getElementById("total-cost"),
       anInitialFee = document.getElementById("an-initial-fee"),
       creditTerm = document.getElementById("credit-term");
+
+
+// variables for range inputs
+
+const totalCostRange = document.getElementById("total-cost-range"),
+	  anInitialFeeRange = document.getElementById("an-initial-fee-range"),
+	  creditTermRange = document.getElementById("credit-term-range");
+
+
+//variables for the results
+
+const amountOfCredit = document.getElementById("amount-of-credit"),
+      monthlyPayment = document.getElementById("monthly-payment"),
+      recommendedIncome = document.getElementById("recommended-income");
+
