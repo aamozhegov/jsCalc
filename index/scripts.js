56638dfb15b4794f34d222d2a3e837433ba27743
Artem Mozhegov <amozhegov@yahoo.com>
2021-07-14 00:57:58 +0300

    Updated scripts.js
    Added function that saves chosen language

--- index/scripts.js
+++ index/scripts.js
@@ -42,7 +42,9 @@ var arrLang = {
 }
 
 
+document.addEventListener("DOMContentLoaded", getLocalLang)
 
+// set language feature
 
 $(function() {
     $(".translate").click(function() {
@@ -54,4 +56,35 @@ $(function() {
     });
 });
 
+
+
+// local storage for language saving
+
+function saveLocalLang(language) {
+    let langs
+    if (localStorage.getItem("langs") === null) {
+        langs = []
+    } else {
+        langs = JSON.parse(localStorage.getItem("langs"))
+    }
+    langs.push(language)
+    localStorage.setItem("langs", JSON.stringify(langs))
+}
+
+function getLocalLang() {
+    let langs
+    if (localStorage.getItem("langs") === null) {
+        langs = []
+    } else {
+        langs = JSON.parse(localStorage.getItem("langs"))
+    }
+    langs.forEach(function(language) {
+        let lang = langs[langs.length - 1]
+        setTimeout(() => {
+            $('.lang').each(function(index, item) {
+                $(this).text(arrLang[lang][$(this).attr("key")]);
+            });
+        }, 0)
+    })
+}
 
