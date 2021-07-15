158f4e6a965f46769b49a7eeb96ad8c83148f175
Artem Mozhegov <amozhegov@yahoo.com>
2021-07-15 23:48:54 +0300

    Updated the title and the intro
    Made minor changes in the main page

--- index/scripts.js
+++ index/scripts.js
@@ -2,7 +2,7 @@
 
 var arrLang = {
     "en": {
-        "comp-interest-calc" : "Compound Interest Calculator",
+        "comp-interest-calc" : "The Power of Compound Interest shows how you can really put your money to work!",
         "introduction" : "Compound interest is the addition of interest to the principal sum of money, or in other words, interest on interest. Use our compound interest calculator to create a projection of how much your savings or investments might grow over a period of time using the power of compound interest.",
         "init-capital" : "Initial Investment:",
         "invest-duration" : "Investment Duration (years):",
@@ -11,7 +11,7 @@ var arrLang = {
         "profit" : "Profit:",
     },
     "ru": {
-        "comp-interest-calc" : "Калькулятор сложного процента",
+        "comp-interest-calc" : "Сила сложного процента показывает, как действительно можно заставить свои деньги работать!",
         "introduction" : "Сложные проценты - это добавление процентов к основной сумме денег, или, другими словами, проценты по процентам. Используйте наш калькулятор сложных процентов, чтобы составить прогноз того, насколько ваши сбережения или инвестиции могут вырасти за определенный период времени, используя силу сложных процентов.",
         "init-capital" : "Первоначальный Взнос:",
         "invest-duration" : "Срок:",
@@ -21,7 +21,7 @@ var arrLang = {
     },
 
     "de": {
-        "comp-interest-calc" : "Zinseszinsrechner",
+        "comp-interest-calc" : "Die Macht des Zinseszinses zeigt, wie Sie Ihr Geld wirklich einsetzen können!",
         "introduction" : "Der Zinseszins ist die Aufstockung der Zinsen auf den Kapitalbetrag, also Zinsen auf Zinsen. Verwenden Sie unseren Zinseszinsrechner, um eine Prognose zu erstellen, wie viel Ihre Ersparnisse oder Investitionen über einen bestimmten Zeitraum mit der Kraft des Zinseszinses wachsen könnten.",
         "init-capital" : "Anfangskapital:",
         "invest-duration" : "Anlagezeitraum:",
@@ -31,7 +31,7 @@ var arrLang = {
     },
 
     "esp": {
-        "comp-interest-calc" : "calculadora de interés compuesto",
+        "comp-interest-calc" : "¡El poder del interés compuesto muestra cómo puede realmente poner su dinero a trabajar!",
         "introduction" : "El interés compuesto es la suma de los intereses a la suma principal de dinero, o en otras palabras, los intereses sobre los intereses. Utilice nuestra calculadora de interés compuesto para crear una proyección de cuánto podrían crecer sus ahorros o inversiones durante un período de tiempo utilizando el poder del interés compuesto.",
         "init-capital" : "Inversión inicial:",
         "invest-duration" : "Duración de la inversión (años):",
